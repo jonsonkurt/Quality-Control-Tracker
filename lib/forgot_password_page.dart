df42072c07d9adbcc84aeac15d881e0afa76a2bd
DavePresbitero <davepresbitero03@gmail.com>
2023-06-15 01:29:21 +0800

    Forgot password UI update

--- lib/forgot_password_page.dart
+++ lib/forgot_password_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:quality_control_tracker/sign_in_page.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({Key? key}) : super(key: key);
@@ -28,25 +29,107 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Forgot Password'),
+        title: const Text(''),
+        elevation: 0,
+        backgroundColor: const Color(0xFFDCE4E9),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignInPage()),
+            );
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Color(0xFF221540),
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+      body: Container(
+        color: const Color(0xFFDCE4E9),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+
+            children: [
+                    const SizedBox(height: 50),  
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "Forgot",
+                        style: TextStyle(
+                          fontFamily: "Rubik-Bold",
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF221540),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "Password",
+                        style: TextStyle(
+                          fontFamily: "Rubik-Bold",
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF221540),
+                        ),
+                      ),
+                    ),
+              const SizedBox(height: 35),      
+              Card(
+                elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                child: TextField(
+                  controller: _emailController,
+                  style: const TextStyle(color: Colors.black),
+                  decoration:  InputDecoration(
+                    hintText: 'Email',
+                    labelStyle:  const TextStyle(
+                            fontFamily: "Karla-Regular",
+                              fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 16, // Adjust the vertical padding here
+                            horizontal: 24, // Adjust the horizontal padding here
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _sendResetLink,
-              child: const Text('Send Reset Link'),
-            ),
-          ],
+              const SizedBox(height: 25),
+              ElevatedButton(
+                onPressed: _sendResetLink,
+                style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(200, 50),
+                          backgroundColor: const Color(0xFF221540),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                30), // Adjust the radius as needed
+                          ),
+                        ),
+                child: const Text(
+                  'Send Reset Link',
+                  style: TextStyle(
+                            fontFamily: "Karla-Bold",
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
