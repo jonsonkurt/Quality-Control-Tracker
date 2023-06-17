4e08342e40db5a65ab8b0c05f3dcee9c2efeb012
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-06-17 16:36:50 +0800

    added floating action on RP's update page

--- lib/view/responsible_party/responsible_party_update_page.dart
+++ lib/view/responsible_party/responsible_party_update_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class ResponsiblePartyUpdatePage extends StatefulWidget {
   final String projectIDQuery;
@@ -15,20 +18,27 @@ class ResponsiblePartyUpdatePage extends StatefulWidget {
 
 class _ResponsiblePartyUpdatePageState
     extends State<ResponsiblePartyUpdatePage> {
+  String? userID = FirebaseAuth.instance.currentUser?.uid;
+  var logger = Logger();
+  String? inspectorID;
+  String? rpFullName;
+  String? rpRole;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFFDCE4E9),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          automaticallyImplyLeading: false,
-          title: const Text(
-            'Updates',
-            style: TextStyle(
-              color: Color(0xFF221540),
-            ),
+      backgroundColor: const Color(0xFFDCE4E9),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Updates',
+          style: TextStyle(
+            color: Color(0xFF221540),
           ),
         ),
-        body: const Text('Responsible party list of updates'));
+      ),
+      body: const Text('Responsible party list of updates'),
+    );
   }
 }
