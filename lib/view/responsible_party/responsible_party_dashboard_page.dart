ebf6f779b3a7cea6c79d8d5fb73011bb90ecc30f
DeVeraGiovanni <giovanni.devera@cvsu.edu.ph>
2023-06-17 20:01:15 +0800

    Fix an error by adding "};"

--- lib/view/responsible_party/responsible_party_dashboard_page.dart
+++ lib/view/responsible_party/responsible_party_dashboard_page.dart
@@ -161,11 +161,8 @@ class _ResponsiblePartyDashboardPageState
           backgroundColor: Colors.white,
           automaticallyImplyLeading: false,
           title: Padding(
-            padding: EdgeInsets.fromLTRB(
-              0, 
-              mediaQuery.size.height * 0.035, 
-              mediaQuery.size.width * 0.06, 
-              0),
+            padding: EdgeInsets.fromLTRB(0, mediaQuery.size.height * 0.035,
+                mediaQuery.size.width * 0.06, 0),
             child: Text(
               'Dashboard',
               style: TextStyle(
@@ -194,99 +191,98 @@ class _ResponsiblePartyDashboardPageState
                   );
                 },
                 icon: Icon(
-                      Icons.account_circle,
-                      size: mediaQuery.size.height * 0.045,
-                      color: const Color(0xFF221540),
-                    ),
+                  Icons.account_circle,
+                  size: mediaQuery.size.height * 0.045,
+                  color: const Color(0xFF221540),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        body: FirebaseAnimatedList(
-          query: FirebaseDatabase.instance
-              .ref()
-              .child('projects/')
-              .orderByChild(rpRole) // Try getting the role upon sign in
-              .equalTo(userID),
-          itemBuilder: (context, snapshot, animation, index) {
-            // Extract project details from the snapshot
-            String projectName = snapshot.child('projectName').value.toString();
-            String projectLocation =
-                snapshot.child('projectLocation').value.toString();
-            String projectDeadline =
-                snapshot.child('projectDeadline').value.toString();
-            String projectStatus =
-                snapshot.child('projectStatus').value.toString();
+      ),
+      body: FirebaseAnimatedList(
+        query: FirebaseDatabase.instance
+            .ref()
+            .child('projects/')
+            .orderByChild(rpRole) // Try getting the role upon sign in
+            .equalTo(userID),
+        itemBuilder: (context, snapshot, animation, index) {
+          // Extract project details from the snapshot
+          String projectName = snapshot.child('projectName').value.toString();
+          String projectLocation =
+              snapshot.child('projectLocation').value.toString();
+          String projectDeadline =
+              snapshot.child('projectDeadline').value.toString();
+          String projectStatus =
+              snapshot.child('projectStatus').value.toString();
 
-            return Padding(
-              padding: const EdgeInsets.only(
-                  top: 20, bottom: 20, left: 10, right: 10),
-              child: GestureDetector(
-                onTap: () {
-                  // ignore: use_build_context_synchronously
-                  Navigator.push<void>(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ResponsiblePartyBottomNavigation(
-                        projectID: snapshot.child('projectID').value.toString(),
-                      ),
+          return Padding(
+            padding:
+                const EdgeInsets.only(top: 20, bottom: 20, left: 10, right: 10),
+            child: GestureDetector(
+              onTap: () {
+                // ignore: use_build_context_synchronously
+                Navigator.push<void>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResponsiblePartyBottomNavigation(
+                      projectID: snapshot.child('projectID').value.toString(),
                     ),
-                  );
-                },
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Project Name: $projectName',
-                          style: TextStyle(
-                              fontFamily: 'Rubik Bold',
-                              fontSize: mediaQuery.size.height * 0.02,
-                              color: const Color(0xFF221540)),
-                        ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Location: $projectLocation',
-                          style: TextStyle(
-                              fontFamily: 'Karla Regular',
-                              fontSize: mediaQuery.size.height * 0.02,
-                              color: const Color(0xFF221540)),
-                        ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Deadline: $projectDeadline',
-                          style: TextStyle(
-                              fontFamily: 'Karla Regular',
-                              fontSize: mediaQuery.size.height * 0.02,
-                              color: const Color(0xFF221540)),
-                        ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Status: $projectStatus',
-                          style: TextStyle(
-                              fontFamily: 'Karla Regular',
-                              fontSize: mediaQuery.size.height * 0.02,
-                              color: const Color(0xFF221540)),
-                        ),
-                      ],
-                    ),
+                );
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Project Name: $projectName',
+                        style: TextStyle(
+                            fontFamily: 'Rubik Bold',
+                            fontSize: mediaQuery.size.height * 0.02,
+                            color: const Color(0xFF221540)),
+                      ),
+                      SizedBox(height: mediaQuery.size.height * 0.01),
+                      Text(
+                        'Location: $projectLocation',
+                        style: TextStyle(
+                            fontFamily: 'Karla Regular',
+                            fontSize: mediaQuery.size.height * 0.02,
+                            color: const Color(0xFF221540)),
+                      ),
+                      SizedBox(height: mediaQuery.size.height * 0.01),
+                      Text(
+                        'Deadline: $projectDeadline',
+                        style: TextStyle(
+                            fontFamily: 'Karla Regular',
+                            fontSize: mediaQuery.size.height * 0.02,
+                            color: const Color(0xFF221540)),
+                      ),
+                      SizedBox(height: mediaQuery.size.height * 0.01),
+                      Text(
+                        'Status: $projectStatus',
+                        style: TextStyle(
+                            fontFamily: 'Karla Regular',
+                            fontSize: mediaQuery.size.height * 0.02,
+                            color: const Color(0xFF221540)),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _showDialog,
-          backgroundColor: const Color(0xFF221540),
-          child: const Icon(Icons.add),
-        ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showDialog,
+        backgroundColor: const Color(0xFF221540),
+        child: const Icon(Icons.add),
       ),
     );
   }
