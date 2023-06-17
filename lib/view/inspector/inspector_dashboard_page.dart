663cc5e1bebdbce7876c48798bb554368c027b7c
DeVeraGiovanni <giovanni.devera@cvsu.edu.ph>
2023-06-17 21:12:04 +0800

    added projects for inspector

--- lib/view/inspector/inspector_dashboard_page.dart
+++ lib/view/inspector/inspector_dashboard_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:quality_control_tracker/view/inspector/inspector_bottom_navigation_bar.dart';
 
@@ -15,6 +14,7 @@ class InspectorDashboardPage extends StatefulWidget {
 
 class _InspectorDashboardPageState extends State<InspectorDashboardPage> {
   String? userID = FirebaseAuth.instance.currentUser?.uid;
+  DatabaseReference ref = FirebaseDatabase.instance.ref().child('projects');
 
   @override
   Widget build(BuildContext context) {
@@ -23,143 +23,133 @@ class _InspectorDashboardPageState extends State<InspectorDashboardPage> {
       onWillPop: () async {
         return false; // Disable back button
       },
-      
       child: Scaffold(
-        backgroundColor: const Color(0xFFDCE4E9),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(
-            mediaQuery.size.height * 0.1
-          ),
-          child: AppBar(
-            backgroundColor: Colors.white,
-            automaticallyImplyLeading: false,
-            title: Padding(
-            padding: EdgeInsets.fromLTRB(
-              0, 
-              mediaQuery.size.height * 0.035, 
-              mediaQuery.size.width * 0.06, 
-              0),
-            child: Text(
-              'Dashboard',
-              style: TextStyle(
-                fontFamily: 'Rubik Bold',
-                fontSize: mediaQuery.size.height * 0.04,
-                color: const Color(0xFF221540),
-              ),
-            ),
-          ),
-            actions: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(
-                0,
-                mediaQuery.size.height * 0.017,
-                mediaQuery.size.width * 0.035,
-                0,
-              ),
-                child: IconButton(
-                  onPressed: () {
-                    // ignore: use_build_context_synchronously
-                    Navigator.push<void>(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const InspectorProfilePage(),
-                      ),
-                    );
-                  },
-                  icon: Icon(
-                    Icons.account_circle,
-                    size: mediaQuery.size.height * 0.045,
+          backgroundColor: const Color(0xFFDCE4E9),
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(mediaQuery.size.height * 0.1),
+            child: AppBar(
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              title: Padding(
+                padding: EdgeInsets.fromLTRB(0, mediaQuery.size.height * 0.035,
+                    mediaQuery.size.width * 0.06, 0),
+                child: Text(
+                  'Dashboard',
+                  style: TextStyle(
+                    fontFamily: 'Rubik Bold',
+                    fontSize: mediaQuery.size.height * 0.04,
                     color: const Color(0xFF221540),
                   ),
                 ),
               ),
-            ],
-          ),
-        ),
-        body: FirebaseAnimatedList(
-          query: FirebaseDatabase.instance
-              .ref()
-              .child('projects/')
-              .orderByChild('inspectorQuery')
-              .equalTo(userID),
-          itemBuilder: (context, snapshot, animation, index) {
-            // Extract project details from the snapshot
-            String projectName = snapshot.child('projectName').value.toString();
-            String projectLocation =
-                snapshot.child('projectLocation').value.toString();
-            String projectDeadline =
-                snapshot.child('projectDeadline').value.toString();
-            String projectStatus =
-                snapshot.child('projectStatus').value.toString();
-
-            return Padding(
-              padding: const EdgeInsets.only(
-                  top: 20, bottom: 20, left: 10, right: 10),
-              child: GestureDetector(
-                onTap: () {
-                  // ignore: use_build_context_synchronously
-                  Navigator.push<void>(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => InspectorBottomNavigation(
-                        projectID: snapshot.child('projectID').value.toString(),
-                      ),
-                    ),
-                  );
-                },
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    0,
+                    mediaQuery.size.height * 0.017,
+                    mediaQuery.size.width * 0.035,
+                    0,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Project Name: $projectName',
-                          style: TextStyle(
-                            fontFamily: 'Rubik Bold',
-                            fontSize: mediaQuery.size.height * 0.02,
-                            color: const Color(0xFF221540)
-                            ),
+                  child: IconButton(
+                    onPressed: () {
+                      // ignore: use_build_context_synchronously
+                      Navigator.push<void>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const InspectorProfilePage(),
                         ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Location: $projectLocation',
-                          style: TextStyle(
-                            fontFamily: 'Karla Regular',
-                            fontSize: mediaQuery.size.height * 0.02,
-                            color: const Color(0xFF221540),
-                        ),
-                          ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Deadline: $projectDeadline',
-                          style: TextStyle(
-                            fontFamily: 'Karla Regular',
-                            fontSize: mediaQuery.size.height * 0.02,
-                            color: const Color(0xFF221540),
-                            ),
-                          ),
-                        SizedBox(height: mediaQuery.size.height * 0.01),
-                        Text(
-                          'Status: $projectStatus',
-                          style: TextStyle(
-                            fontFamily: 'Karla Regular',
-                            fontSize: mediaQuery.size.height * 0.02,
-                            color: const Color(0xFF221540)
-                              ),
-                            ),
-                      ],
+                      );
+                    },
+                    icon: Icon(
+                      Icons.account_circle,
+                      size: mediaQuery.size.height * 0.045,
+                      color: const Color(0xFF221540),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
-        ),
-      ),
+              ],
+            ),
+          ),
+          body: StreamBuilder(
+              stream:
+                  ref.orderByChild("inspectorQuery").equalTo(userID).onValue,
+              builder: (context, AsyncSnapshot snapshot) {
+                dynamic values;
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasData) {
+                  DataSnapshot dataSnapshot = snapshot.data!.snapshot;
+
+                  if (dataSnapshot.value != null) {
+                    values = dataSnapshot.value;
+
+                    return ListView.builder(
+                      itemCount: values.length,
+                      itemBuilder: (context, index) {
+                        String projectID = values.keys.elementAt(index);
+
+                        String projectName = values[projectID]["projectName"];
+                        String projectLocation =
+                            values[projectID]["projectLocation"];
+                        String projectInspector =
+                            values[projectID]["inspector"];
+                        String projectImage = values[projectID]["projectImage"];
+
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => InspectorBottomNavigation(
+                                  projectID: projectID,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      if (projectImage == "None")
+                                        const Text(
+                                          "NO FUCKING \nIMAGE",
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      else
+                                        Image.network(
+                                          projectImage,
+                                          width: 100,
+                                          height: 100,
+                                        ),
+                                      Column(
+                                        children: [
+                                          Text('Project Name: $projectName'),
+                                          Text(
+                                              'Project Location: $projectLocation'),
+                                          Text(
+                                              'Project Inspector: $projectInspector'),
+                                          Text('Project ID: $projectID'),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              // You can access and display other properties from projectData here
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                }
+                return const Text("");
+              })),
     );
   }
 }
