170299d9c971a8cccc095edcb0da58b799b25e5f
Jiiroo19 <jhonheron.carsocho@gmail.com>
2023-06-18 20:06:56 +0800

    removed unused import

--- lib/view/responsible_party/responsible_party_profile_page.dart
+++ lib/view/responsible_party/responsible_party_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../../sign_in_page.dart';
