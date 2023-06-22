c08c79b54026fb431b94b07882a7774793b5637a
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-06-22 18:15:09 +0800

    added project information page

--- lib/view/inspector/inspector_home_page.dart
+++ lib/view/inspector/inspector_home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:quality_control_tracker/image_viewer.dart';
 
 class InspectorHomePage extends StatefulWidget {
   final String projectIDQuery;
@@ -15,48 +17,307 @@ class InspectorHomePage extends StatefulWidget {
 class _InspectorHomePageState extends State<InspectorHomePage> {
   @override
   Widget build(BuildContext context) {
-
     final mediaQuery = MediaQuery.of(context);
 
+    DatabaseReference projectDetailsRef = FirebaseDatabase.instance
+        .ref()
+        .child('projects/${widget.projectIDQuery}');
+
     return Scaffold(
-        backgroundColor: const Color(0xFFDCE4E9),
-        appBar: PreferredSize(
-          preferredSize:  Size.fromHeight(
+      backgroundColor: const Color(0xFFDCE4E9),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
           mediaQuery.size.height * 0.1,
+        ),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          leading: Padding(
+            padding: EdgeInsets.fromLTRB(
+              mediaQuery.size.width * 0.035,
+              mediaQuery.size.height * 0.028,
+              0,
+              0,
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Color(0xFF221540),
+              ),
+            ),
           ),
-          child: AppBar(
-            backgroundColor: Colors.white,
-            leading: Padding(
-              padding: EdgeInsets.fromLTRB(
-                mediaQuery.size.width * 0.035,
-                mediaQuery.size.height * 0.028,
-                0,
-                0,
-                ),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  }, 
-                  icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Color(0xFF221540),),
-                    ),
-                    ),
-            title: Padding(
-              padding: EdgeInsets.only(
-                top: mediaQuery.size.height * 0.035,
-                ),
-              child: Text(
-                'Information',
-                style: TextStyle(
-                  fontFamily: 'Rubik Bold',
-                  fontSize: mediaQuery.size.height * 0.04,
-                  color: const Color(0xFF221540),
-                ),
+          title: Padding(
+            padding: EdgeInsets.only(
+              top: mediaQuery.size.height * 0.035,
+            ),
+            child: Text(
+              'Information',
+              style: TextStyle(
+                fontFamily: 'Rubik Bold',
+                fontSize: mediaQuery.size.height * 0.04,
+                color: const Color(0xFF221540),
               ),
             ),
           ),
         ),
-        body: const Text('Inspector information'));
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: StreamBuilder(
+            stream: projectDetailsRef.onValue,
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasData) {
+                // Getting values from database
+                Map<dynamic, dynamic> map = snapshot.data.snapshot.value;
+
+                String projectImage = map['projectImage'];
+                String projectName = map['projectName'];
+                String inspectorName = map['inspector'];
+                String projectDeadline = map['projectDeadline'];
+                String owner = map['owner'];
+
+                String projectManager = map['projectManager'];
+                String plumber = map['plumber'];
+                String painter = map['painter'];
+                String mason = map['mason'];
+                String laborer = map['laborer'];
+                String electrician = map['electrician'];
+                String welder = map['welder'];
+                String carpenter = map['carpenter'];
+                String landscaper = map['landscaper'];
+                String hvac = map['HVAC'];
+                String technician = map['technician'];
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return DetailScreen(
+                              imageUrl: projectImage,
+                              projectID: widget.projectIDQuery,
+                            );
+                          }));
+                        },
+
+                        // Image (kindly consult Jiiroo if you can't understand the code ty. ヾ(≧▽≦*)o)
+                        child: Hero(
+                          tag: widget.projectIDQuery,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: projectImage == "None"
+                                ? Image.asset(
+                                    'assets/images/no-image.png',
+                                    fit: BoxFit.cover,
+                                    width: 300,
+                                    height: 200,
+                                  )
+                                : Image(
+                                    width: 300,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(projectImage),
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      }
+                                      return const CircularProgressIndicator();
+                                    },
+                                    errorBuilder: (context, object, stack) {
+                                      return const Icon(
+                                        Icons.error_outline,
+                                        color: Color.fromARGB(255, 35, 35, 35),
+                                      );
+                                    },
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Project Name: $projectName",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'Inspector In-Charge: $inspectorName',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        "Project Deadline: $projectDeadline",
+                        // "Submission Date: ",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        "Project Owner: $owner",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        "Project Team:",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Project Manager: $projectManager",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Plumber: $plumber",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Painter: $painter",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Mason: $mason",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Laborer: $laborer",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Electrician: $electrician",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Welder: $welder",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Carpenter: $carpenter",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Landscaper: $landscaper",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "HVAC: $hvac",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Technician: $technician",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(
+                    child: Text(
+                  'Something went wrong.',
+                ));
+              }
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
