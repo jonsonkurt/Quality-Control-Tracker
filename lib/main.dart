762ccd09f11e53a7fd0ca5d7f02b58a7281db732
PacomioJohnEmmanuel <johnemmanuel.pacomio@cvsu.edu.ph>
2023-06-25 13:44:53 +0800

    updated primarySwatch

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,26 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,7 +46,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Quality Control Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: createMaterialColor(const Color(0xFF221540)),
       ),
       routes: {
         '/': (context) => const WelcomePage(),
